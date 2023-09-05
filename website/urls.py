a2b3a910b53e045a25762bf76bd69f0fabcc64f4
Mohammad Afsharian <mafsharian45@gmail.com>
2023-09-05 13:39:49 +0330

    update urls.py
    remove index

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from website.views import *
 app_name ='website'
 
 urlpatterns = [
-    path('index', index_view, name='index'),
+    path('', index_view, name='index'),
     path('inner', inner_view, name='inner'),
     path('portfolio', portfolio_view, name='portfolio'),
 ]
